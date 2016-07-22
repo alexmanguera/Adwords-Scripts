1b161f843b561ac5f2b41f8c3e57945b4cc98949
alex manguera <alexmanguera@gmail.com>
2016-07-22 20:09:47 +0800

    - Allows to Compute for New Bid based on either ROAS method or conventional method. - Select a method via the constant "var SELECTED_BID_METHOD".

--- Bid Adjustment/Bid Adjustment (DEV).js
+++ Bid Adjustment/Bid Adjustment (DEV).js
@@ -2,7 +2,7 @@
 
 ====================
 Bid Adjustment
-Current Version: 1.2
+Current Version: 1.3
 ====================
 
 Change Log:
@@ -12,17 +12,33 @@ ver 1.1
 - Apply value of upper-bid-limit (Max CPC) if new-bid is greater than upper-bid-limit.
 ver 1.2
 - Make use of array for Campaigns to allow processing of multiple Campaigns.
+ver 1.3
+- Allows to Compute for New Bid based on either ROAS method or conventional method.
 
 **************/
 
 var TARGET_CPA = 35;
 var UPPER_BID_LIMIT = 10;
+var TARGET_ROAS = 4;
 
+var SEVEN_DAYS = "LAST_7_DAYS";
+var FOURTEEN_DAYS = "LAST_14_DAYS";
+var THIRTY_DAYS = "LAST_30_DAYS";
+var ALL_TIME = "ALL_TIME";
+
+// ---------------------------
+// A = ROAS based.
+// B = conventional.
+var SELECTED_BID_METHOD = "A";
+// ---------------------------
+
+// ---------------------------
 // provide the campaign names you need to process within the array.
 var CAMPAIGN_NAME = [
-					"AU - Bali Wedding (BMM)",
-					"AU - Villa Botanica"
+					"AU > Brand Terms (BMM)",
+					"AU > Generic Terms (BMM)"
 					];
+// ---------------------------
 
 function main()
 {
@@ -41,10 +57,19 @@ function main()
 			{
 				var campaign = campaignIterator.next();
 				
+				if(SELECTED_BID_METHOD == "A")
+				{
+					dispMethod = "ROAS based."
+				}else{
+					dispMethod = "Conventional."
+				}
+				
 				Logger.log("===================================");
+				Logger.log("Selected Method: " + dispMethod);
 				Logger.log("Campaign Name: " + campaign.getName());
 				Logger.log("Upper Bid Limit: " + UPPER_BID_LIMIT);
 				Logger.log("Target CPA: " + TARGET_CPA);
+				Logger.log("Target ROAS: " + TARGET_ROAS);
 				Logger.log("===================================");
 				Logger.log("\n");
 								
@@ -64,10 +89,10 @@ function main()
 						var AdGroup = keywords.getAdGroup().getName();
 						var currentCPC = keywords.bidding().getCpc();
 						
-						var stats_seven_days = keywords.getStatsFor('LAST_7_DAYS');
-						var stats_fourteen_days = keywords.getStatsFor('LAST_14_DAYS');
-						var stats_thirty_days = keywords.getStatsFor('LAST_30_DAYS');
-						var stats_all_time = keywords.getStatsFor('ALL_TIME');
+						var stats_seven_days = keywords.getStatsFor(SEVEN_DAYS);
+						var stats_fourteen_days = keywords.getStatsFor(FOURTEEN_DAYS);
+						var stats_thirty_days = keywords.getStatsFor(THIRTY_DAYS);
+						var stats_all_time = keywords.getStatsFor(ALL_TIME);
 						
 						// 7 days
 						var a_averagecpc = stats_seven_days.getAverageCpc();
@@ -125,45 +150,117 @@ function main()
 						var d_ctr = stats_all_time.getCtr();
 						var d_impressions = stats_all_time.getImpressions();
 						
+						
+						// --------------------------
+						var roas_method = false;
+						if(SELECTED_BID_METHOD == "A")
+						{
+							// ROAS
+							var roas_method = true;
+						}else{
+							
+							var roas_method = false;
+						}
+						// --------------------------
+						
+						
 						// --------------------------
 						// 7 Days - Cost / Conversion AND Bid result
-						var a_cost_over_conversion = getCostOverConversion(a_cost, a_convertedclicks);
-						var a_bid_result = getBidResult(TARGET_CPA, a_cost_over_conversion, a_averagecpc);
-						
-						// 14 Days - Cost / Conversion AND Bid result
-						var b_cost_over_conversion = getCostOverConversion(b_cost, b_convertedclicks);
-						var b_bid_result = getBidResult(TARGET_CPA, b_cost_over_conversion, b_averagecpc);
-						
-						// 30 Days - Cost / Conversion AND Bid result
-						var c_cost_over_conversion = getCostOverConversion(c_cost, c_convertedclicks);
-						var c_bid_result = getBidResult(TARGET_CPA, c_cost_over_conversion, c_averagecpc);
-						
-						// All Time - Cost / Conversion AND Bid result
-						var d_cost_over_conversion = getCostOverConversion(d_cost, d_convertedclicks);
-						var d_bid_result = getBidResult(TARGET_CPA, d_cost_over_conversion, d_averagecpc);
+						if(!roas_method)
+						{
+							var a_cost_over_conversion = getCostOverConversion(a_cost, a_convertedclicks);
+							var a_bid_result = getBidResult(TARGET_CPA, a_cost_over_conversion, a_averagecpc);
+						}
+						else
+						{
+							// ROAS
+							var a_conversion_value_over_cost = getConversionValueOverCost(campaign.getName(), keyword, SEVEN_DAYS);
+							var a_bid_result_roas = getBidResultRoas(TARGET_ROAS, a_conversion_value_over_cost, a_averagecpc);
+						}
 						// --------------------------
 						
-						var bid_array = [a_bid_result, b_bid_result, c_bid_result, d_bid_result];
+						// --------------------------
+						// 14 Days - Cost / Conversion AND Bid result
+						if(!roas_method)
+						{
+							var b_cost_over_conversion = getCostOverConversion(b_cost, b_convertedclicks);
+							var b_bid_result = getBidResult(TARGET_CPA, b_cost_over_conversion, b_averagecpc);
+						}
+						else
+						{
+							// ROAS
+							var b_conversion_value_over_cost = getConversionValueOverCost(campaign.getName(), keyword, FOURTEEN_DAYS);
+							var b_bid_result_roas = getBidResultRoas(TARGET_ROAS, b_conversion_value_over_cost, b_averagecpc);
+						}
+						// --------------------------
 						
-						var new_bid = getNewBid(bid_array);
+						// --------------------------
+						// 30 Days - Cost / Conversion AND Bid result
+						if(!roas_method)
+						{
+							var c_cost_over_conversion = getCostOverConversion(c_cost, c_convertedclicks);
+							var c_bid_result = getBidResult(TARGET_CPA, c_cost_over_conversion, c_averagecpc);
+						}
+						else
+						{
+							// ROAS
+							var c_conversion_value_over_cost = getConversionValueOverCost(campaign.getName(), keyword, THIRTY_DAYS);
+							var c_bid_result_roas = getBidResultRoas(TARGET_ROAS, c_conversion_value_over_cost, c_averagecpc);
+						}
+						// --------------------------
+						
+						// --------------------------
+						// All Time - Cost / Conversion AND Bid result
+						if(!roas_method)
+						{
+							var d_cost_over_conversion = getCostOverConversion(d_cost, d_convertedclicks);
+							var d_bid_result = getBidResult(TARGET_CPA, d_cost_over_conversion, d_averagecpc);
+						}
+						else
+						{
+							// ROAS
+							var d_conversion_value_over_cost = getConversionValueOverCost(campaign.getName(), keyword, ALL_TIME);
+							var d_bid_result_roas = getBidResultRoas(TARGET_ROAS, d_conversion_value_over_cost, d_averagecpc);
+						}
+						// --------------------------
+						
+						
+						var bid_array = [a_bid_result, b_bid_result, c_bid_result, d_bid_result];
+						// ROAS
+						var bid_array_roas = [a_bid_result_roas, b_bid_result_roas, c_bid_result_roas, d_bid_result_roas];
+						
+	
+						// --------------------------
+						if(!roas_method)
+						{
+							var new_bid = getNewBid(bid_array);
+							finalOutputNewBid = new_bid;
+						}
+						else
+						{
+							// ROAS
+							var new_bid_roas = getNewBid(bid_array_roas);
+							finalOutputNewBid = new_bid_roas;
+						}
+						// --------------------------
 						
 						var errorLog = "";
 						var validNewBid = false;
 						
 						
 						// use upper-bid-limit value if new-bid is greater than upper-bid-limit.
-						if(new_bid > UPPER_BID_LIMIT)
+						if(finalOutputNewBid > UPPER_BID_LIMIT)
 						{
-							Logger.log("Notice!... New Bid ($"+new_bid+") is above Upper-Bid-Limit. Using Upper-Bid-Limit as the New-Bid.");
-							new_bid = UPPER_BID_LIMIT;
+							Logger.log("Notice!... New Bid ($"+finalOutputNewBid+") is above Upper-Bid-Limit. Using Upper-Bid-Limit as the New-Bid.");
+							finalOutputNewBid = UPPER_BID_LIMIT;
 							validNewBid = true;
 						}
-						else if(new_bid > TARGET_CPA)
+						else if(finalOutputNewBid > TARGET_CPA)
 						{
-							Logger.log("Skipping!... New Bid ($"+new_bid+") is above Target CPA.");
+							Logger.log("Skipping!... New Bid ($"+finalOutputNewBid+") is above Target CPA.");
 							validNewBid = false;
 						}
-						else if(new_bid == 0)
+						else if(finalOutputNewBid == 0)
 						{
 							Logger.log("Skipping!... New Bid is $0.00");
 							validNewBid = false;
@@ -176,13 +273,39 @@ function main()
 						// check to set maxCPC with new bid
 						if(validNewBid)
 						{
-							keywords.setMaxCpc(new_bid);
+							keywords.setMaxCpc(finalOutputNewBid);
 						}
 						
-					Logger.log(keyword + "\n:: 7 Day Bid = " + a_bid_result + " | 14 Day Bid = " + b_bid_result + " | 30 Day Bid = " + c_bid_result + " | All Time = " + d_bid_result + " | [Current Bid = "+ currentCPC +" **New Bid = " + new_bid + "]");
-						Logger.log("\n");
 						
-					}
+						// ------------------------------------------------------------
+						// Display Log
+						if(!roas_method)
+						{
+							Logger.log(
+										keyword + "\n:: 7 Day Bid = " + a_bid_result +
+										" | 14 Day Bid = " + b_bid_result +
+										" | 30 Day Bid = " + c_bid_result +
+										" | All Time = " + d_bid_result +
+										" | [Current Bid = " + currentCPC + 
+										" **New Bid = " + finalOutputNewBid
+									  );
+						}
+						else
+						{
+							Logger.log(
+										keyword + "\n:: 7 Day Bid = " + a_bid_result_roas +
+										" | 14 Day Bid = " + b_bid_result_roas +
+										" | 30 Day Bid = " + c_bid_result_roas +
+										" | All Time = " + d_bid_result_roas +
+										" | [Current Bid = " + currentCPC + 
+										" **New Bid = " + finalOutputNewBid
+									  );
+							
+						}
+						Logger.log("\n");
+						// ------------------------------------------------------------
+						
+					} // end while
 				}
 			} // end if
 		} // end for loop
@@ -203,15 +326,30 @@ function main()
 	
 	// ------------------------------------
 	// get the Bid result.
-	function getBidResult(TARGET_CPA, cost_over_conversion, averagecpc) {
+	function getBidResult(target_cpa, cost_over_conversion, averagecpc) {
 		
 		var output = 0;
 		if(cost_over_conversion != 0)
 		{
-			output = TARGET_CPA / cost_over_conversion;
+			output = target_cpa / cost_over_conversion;
 			output = output * averagecpc;
 		}
 		output = output.toFixed(2);
+		return output;
+	}
+	
+	
+	// ------------------------------------
+	// ROAS
+	// get Conversion/Cost result
+	function getConversionOverCost(cost, convertedclicks) {
+		
+		var output = 0;
+		if(cost != 0 && convertedclicks != 0)
+		{
+			var output = removeCommas(convertedclicks) / removeCommas(cost);
+			output = output * 100;
+		}
 		return output;
 	}
 	
@@ -243,6 +381,65 @@ function main()
 		
 	}
 	
+	// ------------------------------------
+	// ROAS
+	// get the Bid result.
+	function getBidResultRoas(target_roas, conversion_value_over_cost, averagecpc) {
+		
+		var output = 0;
+		if(conversion_value_over_cost != 0)
+		{
+			output = conversion_value_over_cost / target_roas;
+			output = output * averagecpc;
+		}
+		output = output.toFixed(2);
+		return output;
+	}
+	
+	// ------------------------------------
+	// query to retrieve the Conversion Value.	
+	function getConversionValueOverCost(campaignName, keyword, timeline) {
+		
+		// "Criteria" is used to get the Keyword. KeywordText is deprecated.
+		if(timeline == "ALL_TIME")
+		{
+		var report = AdWordsApp.report(
+									'SELECT ConversionValue, Cost ' +
+									'FROM KEYWORDS_PERFORMANCE_REPORT ' +
+									'WHERE CampaignName = "' + campaignName + '" ' +
+									'AND Criteria = "' + keyword + '"'
+									);
+		}
+		else
+		{
+		var report = AdWordsApp.report(
+									'SELECT ConversionValue, Cost ' +
+									'FROM KEYWORDS_PERFORMANCE_REPORT ' +
+									'WHERE CampaignName = "' + campaignName + '" ' +
+									'AND Criteria = "' + keyword + '" ' +
+									'DURING '+ timeline
+									);			
+		}
+		var convValsIterator = report.rows();
+		
+		while(convValsIterator.hasNext())
+		{
+			var row = convValsIterator.next();
+			var outConversionValue = row.ConversionValue;
+			var outCost = row.Cost;
+		}
+		
+		var output = 0;
+		if(outConversionValue != 0 && outCost !=0)
+		{
+			var output = removeCommas(outConversionValue) / removeCommas(outCost);
+			//var output = outConversionValue + " : " + outCost;
+			output = output.toFixed(2);
+		}
+		return output;
+	}
+	
+	
 	
 	// ------------------------------------
 	// HELPER FUNCTION
@@ -251,6 +448,18 @@ function main()
 		
 		return total + num;
 	}
+	
+	// remove commas in numbers.
+	function removeCommas(str) {
+		
+		if(typeof str != "undefined")
+		{
+			while (str.search(",") >= 0) {
+				str = (str + "").replace(',', '');
+			}
+		}
+		return str;
+	};
 	
 	
 	
